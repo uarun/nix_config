03f996ade838351ed3dacdd793b232c48168d984
Arun Udayashankar <arunkumar.u@gmail.com>
2023-05-15 18:11:23 -0400

    Add Mac Finder preferences

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -21,7 +21,15 @@
 
   system.defaults = {
     finder.AppleShowAllExtensions = true;
+    finder.FXEnableExtensionChangeWarning = false;
     finder._FXShowPosixPathInTitle = true;
+    finder.QuitMenuItem = true;
+    finder.FXPreferredViewStyle = "Nlsv"            #... Change default finder view to List View
+    finder.FXDefaultSearchScope = "SCcf"            #... Change default search scope to Current Folder
+    finder.ShowPathbar   = true;
+    finder.ShowStatusbar = true;
+
+    LauchServices.LSQuarantine = true;              #... Enable Quarantine for downloaded applications
 
     dock.autohide = true;
 
