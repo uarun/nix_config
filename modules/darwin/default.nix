fd1e6930c5f5072fc80a65a818e5f060306f0df9
Arun Udayashankar <arunkumar.u@gmail.com>
2023-05-19 20:33:34 -0400

    Externalize homebrew configuration

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -121,40 +121,7 @@
   };
 
   #... All things Homebrew go here
-  homebrew = {
-    enable = true;        #... nix-darwin to manage installing/updating/uprading Homebrew taps
-    onActivation = {
-      autoUpdate = false;
-      upgrade = false;
-      cleanup = "zap";    #... "uninstall" or "zap"
-    };
-    global.brewfile = true;
-
-    #... List of Homebrew formula repositories to tap
-    taps = [
-      "homebrew/cask"
-    ];
-
-    #... List of Homebrew Formulae to install
-    brews = [
-    ];
-
-    #... List of Homebrew Casks to install
-    casks = [ 
-      "brave-browser"
-      "citrix-workspace"
-      "flux"                   # f.lux
-      "keepassxc"
-      "microsoft-teams"
-      "notion"
-      "obsidian"
-      "raycast"
-    ];
-
-    #... List of application to install from the offical Mac App Store (using mas)
-    masApps = {
-    };
-  };
+  homebrew = import ./homebrew.nix pkgs;
 
   #... This is here for backwards compatibility, don't change
   system.stateVersion = 4;
---
+++ modules/darwin/homebrew.nix
@@ -0,0 +1,35 @@
+_:
+{
+  enable = true;        #... nix-darwin to manage installing/updating/uprading Homebrew taps
+  onActivation = {
+    autoUpdate = false;
+    upgrade = false;
+    cleanup = "zap";    #... "uninstall" or "zap"
+  };
+  global.brewfile = true;
+
+  #... List of Homebrew formula repositories to tap
+  taps = [
+    "homebrew/cask"
+  ];
+
+  #... List of Homebrew Formulae to install
+  brews = [
+  ];
+
+  #... List of Homebrew Casks to install
+  casks = [ 
+    "brave-browser"
+    "citrix-workspace"
+    "flux"                   # f.lux
+    "keepassxc"
+    "microsoft-teams"
+    "notion"
+    "obsidian"
+    "raycast"
+  ];
+
+  #... List of application to install from the offical Mac App Store (using mas)
+  masApps = {
+  };
+}
