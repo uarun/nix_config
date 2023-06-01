db525a61fa380b8be777537ca0372811855dca85
Arun Udayashankar <arunkumar.u@gmail.com>
2023-05-31 21:36:52 -0400

    Update neovim config to bare-bones

--- modules/home-manager/programs/neovim.nix
+++ modules/home-manager/programs/neovim.nix
@@ -1,7 +1,5 @@
 { pkgs, ... }: {
 
-  home.file.".config/nvim/settings.lua".source = ../nvim/init.lua;
-
   programs.neovim = {
     enable = true;
     viAlias = true;
@@ -13,33 +11,7 @@
     withRuby    = true;
     withPython3 = true;
 
-    #... share vim plugins since nothing is specific to nvim
     plugins = with pkgs.vimPlugins; [
-      #... basics
-      vim-sensible
-      vim-fugitive
-      vim-sandwich
-      vim-commentary
-      vim-nix
-
-      #... UI Plugins
-      telescope-nvim
-      gitsigns-nvim
-      bufferline-nvim
-      toggleterm-nvim
-      (nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars))  # better code coloring
-      nvim-treesitter-textobjects                                      # movement based on syntax
-      nvim-treesitter-context                                          # preserve current context (like func definition, for loop etc.)
-      nvim-web-devicons                                                # pretty icons used by other plugins like lualine
-      lualine-nvim                                                     # status bar
-
-      #... Misc
-      impatient-nvim                   # speeds startup times by caching lua bytecode
-      which-key-nvim
     ];
-
-    extraConfig = ''
-      luafile ~/.config/nvim/settings.lua
-    '';
   };
 }
