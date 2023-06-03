145c0cbf1a93a4752a1b73f3539e75e7777b6c08
Arun Udayashankar <arunkumar.u@gmail.com>
2023-06-03 11:35:26 -0400

    Update kitty configuration

--- modules/home-manager/programs/kitty.nix
+++ modules/home-manager/programs/kitty.nix
@@ -21,6 +21,36 @@
       scrollback_lines = 10000;
       shell_integration = "no-cursor";
       window_padding_width = 3;
+      disable_ligatures = "cursor";         #... disable disable ligatures when the cursor is under them
+
+      #... Tab bar
+      tab_bar_edge  = "top";
+      tab_bar_style = "powerline";
+      tab_title_template = "{index}: {title}";
+      active_tab_font_style = "normal";
+      inactive_tab_font_style = "normal";
+      tab_activity_symbol = "";
     };
+
+    extraConfig = ''
+      #... Key mappings
+      map cmd+c        copy_to_clipboard
+      map cmd+v        paste_from_clipboard
+
+      ##... Window Management
+      map cmd+o         launch --cwd=current --type=window
+      map cmd+n         new_os_window
+      map cmd+w         close_window
+      map cmd+right     next_window
+      map cmd+left      previous_window
+      map cmd+up        move_window_forward
+      map cmd+down      move_window_backward
+      map cmd+l         next_layout
+
+      ##... Tab Management
+      map cmd+enter     launch --cwd=current --type=tab
+      map cmd+alt+right next_tab
+      map cmd+alt+left  previous_tab
+    '';
   };
 }
