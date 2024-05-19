9e4dc8a2d02df5fcda0da2ad828d6da5f141d7fd
Arun Udayashankar <arunkumar.u@gmail.com>
2024-05-19 10:08:46 -0400

    Update renamed zsh option

--- modules/home-manager/programs/zsh.nix
+++ modules/home-manager/programs/zsh.nix
@@ -7,7 +7,7 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    enableAutosuggestions = true;
+    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
     dotDir = ".config/zsh";
