231872293fd98c6f5f5f7a6116e8c45be9d3d28f
Arun Udayashankar <arunkumar.u@gmail.com>
2026-02-01 14:20:33 -0500

    Skip nushell tests because of issues

--- modules/home-manager/programs/nushell.nix
+++ modules/home-manager/programs/nushell.nix
@@ -1,5 +1,8 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.nushell = {
     enable = true;
+    package = pkgs.nushell.overrideAttrs (old: {
+      doCheck = false;  # Skip tests - they fail in nix sandbox due to permission issues
+    });
   };
 }
