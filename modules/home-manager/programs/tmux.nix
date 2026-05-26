673f6a5737f4f15091ca4a8053800ea1ee720de9
Arun Udayashankar <audayashankar@liquidnet.com>
2026-05-26 11:27:17 -0400

    feat(tmux): add plugins, shell, and keybindings
    - Add catppuccin theme and sensible plugins
    - Set zsh as default shell
    - Add config reload, split-window keybindings

--- modules/home-manager/programs/tmux.nix
+++ modules/home-manager/programs/tmux.nix
@@ -8,5 +8,28 @@
     historyLimit = 50000;
     escapeTime = 10;
     terminal = "tmux-256color";
+    shell = "${pkgs.zsh}/bin/zsh";
+
+    plugins = with pkgs; [
+      tmuxPlugins.sensible
+      {
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavour 'mocha'
+          set -g @catppuccin_window_tabs_enabled on
+        '';
+      }
+    ];
+
+    extraConfig = ''
+      ##... Reload config shortcut
+      bind r source-file ~/.config/tmux/tmux.conf \; display "Tmux Config reloaded!"
+
+      ##... Better splitting (mnemonic)
+      unbind %
+      bind | split-window -h -c "#{pane_current_path}"
+      unbind '"'
+      bind - split-window -v -c "#{pane_current_path}"
+    '';
   };
 }
