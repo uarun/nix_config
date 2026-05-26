da10fb86a491c3c16074e694a008a73e33012080
Arun Udayashankar <audayashankar@liquidnet.com>
2026-05-26 14:14:03 -0400

    tmux: add resurrect/continuum plugins and enable extended-keys
    - Add resurrect plugin with vim/nvim session strategy and pane capture
    - Add continuum plugin for auto-save (10min interval) and auto-restore
    - Enable extended-keys for modified key encoding (Shift+Enter, etc.)

--- modules/home-manager/programs/tmux.nix
+++ modules/home-manager/programs/tmux.nix
@@ -19,9 +19,28 @@
           set -g @catppuccin_window_tabs_enabled on
         '';
       }
+      {
+        plugin = tmuxPlugins.resurrect;
+        extraConfig = ''
+          set -g @resurrect-strategy-vim 'session'
+          set -g @resurrect-strategy-nvim 'session'
+          set -g @resurrect-capture-pane-contents 'on'
+          '';
+      }
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-boot 'on'
+          set -g @continuum-save-interval '10'
+          '';
+      }
     ];
 
     extraConfig = ''
+      ##... Enable modified key encoding (Shift+Enter, etc.)
+      set -g extended-keys on
+
       ##... Reload config shortcut
       bind r source-file ~/.config/tmux/tmux.conf \; display "Tmux Config reloaded!"
 
